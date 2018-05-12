cd0e46f7c43cb641ffb84456d4f4b395dfadc9ca
Zarah Parker <parker@civicplus.com>
2018-05-11 23:41:44 -0400

    1

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendData = [
+    {
+    "name":"Dee Reynolds",
+    "photo":"https://media0ch-a.akamaihd.net/25/46/d7bb68957ec787d6673f2c8e05b8e9fb.jpg",
+    "scores":[5,3,2,4,3,1,5,5,4,2]
+    },
+    {
+    "name":"Charlie Kelly",
+    "photo":"https://i0.wp.com/www.pinkink.media/wp-content/uploads/2015/07/dayman-charlie-kelly.jpg?fit=512%2C384&ssl=1",
+    "scores":[3,2,1,4,4,5,2,1,2,4]
+    },
+    {
+    "name":"Tina Belcher",
+    "photo":"https://uproxx.files.wordpress.com/2016/01/mac-banner.jpg?quality=95&w=650&h=360",
+    "scores":[1,2,4,2,5,4,2,3,2,5]
+    },
+  ];
+  
+  module.exports = friendData;
